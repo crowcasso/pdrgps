074e15cbc30d60cc06547cba737a62b24d1d234a
crowcasso <crowcasso@gmail.com>
2012-06-21 10:56:12 -0400

    Created GPSCollector thread. Rewrote DataCollectorActvitity to use the thread.

--- src/edu/elon/cs/collection/DataCollectorActivity.java
+++ src/edu/elon/cs/collection/DataCollectorActivity.java
@@ -1,49 +1,27 @@
 package edu.elon.cs.collection;
 
 /**
- *  Main activity.
+ *  Starts the GPS and Accelerometer threads for data collection.
  * 
- *  @author C. Brockmyre
+ *  @author C. Brockmyre and J. Hollingsworth
  */
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Date;
-
-
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.os.Handler;
 
 public class DataCollectorActivity extends Activity {
 	
-	private LocationManager locManager;
-	private SensorManager manager;
-	private File sensorFile;
-	private File gpsFile;
-	//private String sensorFile;
-	private TextView xaxis;
-	private TextView yaxis;
-	private TextView zaxis;
-	private float x;
-	private float y;
-	private float z;
-	private float orientation;
-	FileOutputStream fos = null;
-	FileOutputStream fos2 = null;
-
+	private TextView gpstv;
+	private TextView acceltv;
+	private Button go;
+	
+	private GPSCollector gpsCollector;
 	
     /** Called when the activity is first created. */
     @Override
@@ -51,133 +29,44 @@ public class DataCollectorActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // UI setup
+        gpstv = (TextView) findViewById(R.id.gpstv);
+        go = (Button) findViewById(R.id.gobutton);
+        go.setOnClickListener(goButtonListener);
+        go.setEnabled(false);
         
-        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
-		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10000.0f, locListener);
-		
-		manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		
-		//sensorFile = "sensorFile"
-		/*try {
-			sensorFile.createNewFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
-		xaxis = (TextView) findViewById(R.id.xaxis);
-		yaxis = (TextView) findViewById(R.id.yaxis);
-		zaxis = (TextView) findViewById(R.id.zaxis);
-		
-		File test = Environment.getExternalStorageDirectory();
-		try {
-			fos = new FileOutputStream(new File(test, "sensorFile.csv"));
-			fos2 = new FileOutputStream(new File(test, "gpsFile.csv"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Log.d("onCreate", getFilesDir().toString());
-
+        /* GPS Thread */
+        gpsCollector = new GPSCollector(getBaseContext(), new Handler() {
+            public void handleMessage(Message m) {
+            	String msg = m.getData().getString("gps-message");
+                gpstv.setText(msg);
+                if (msg.equals("Available")) {
+                	go.setEnabled(true);
+                }
+             }
+           });  
     }
+    
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
     	super.onDestroy();
-    	try {
-			fos.close();
-			fos2.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	
+    	// exit the thread nicely
+    	boolean retry = true;
+        gpsCollector.setRunning(false);
+        while (retry) {
+           try {
+              gpsCollector.join();
+              retry = false;
+           } catch (InterruptedException ex) {}
+        }
     }
     
-	@Override
-	protected void onResume(){
-		super.onResume();
-		manager.registerListener(sensorListener, manager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-				SensorManager.SENSOR_DELAY_FASTEST);
-		manager.registerListener(sensorListener, manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-				SensorManager.SENSOR_DELAY_FASTEST);
-	}
-    
-	@Override
-	protected void onPause(){
-		super.onPause();
-		manager.unregisterListener(sensorListener);
-	}
-    
-    private LocationListener locListener = new LocationListener() {
-
-		public void onLocationChanged(Location location) {
-			// write the time and the gps location to a file
-			String printOut = System.currentTimeMillis() + "," + location.getLatitude() + "," + location.getLongitude() + "," + location.getAccuracy();
-			//String str = System.currentTimeMillis() + "," + x + "," + y + "," + z + "," + orientation + "\n";
-			try {
-				//fos = openFileOutput("sensorFile", Context.MODE_PRIVATE);
-				
-				Log.d("Test", fos2.toString());
-				fos.write(printOut.getBytes());
-				//fos.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				zaxis.setText("didn't write");
-			}
-		}
-
-		//@Override
-		public void onProviderDisabled(String arg0) {
-		}
-
-		//@Override
-		public void onProviderEnabled(String arg0) {
-		}
-
-		//@Override
-		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-		}
-
-    	
+    private OnClickListener goButtonListener = new OnClickListener() {
+		public void onClick(View view) {
+			go.setText("End Data Collection");
+			gpsCollector.setRunning(true);
+			gpsCollector.start();
+		}    	
     };
-
-
-	private SensorEventListener sensorListener = new SensorEventListener(){
-
-		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public void onSensorChanged(SensorEvent event) {
-			// TODO Auto-generated method stub
-			if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-				//write the time and accelerometer data to a file.
-				//get the compass orientation in here too each time i think (aka dont need next if statement)
-				xaxis.setText("X-axis: " + event.values[0]);
-				yaxis.setText("Y-axis: " + event.values[1]);
-				zaxis.setText("Z-axis: " + event.values[2]);
-				x = event.values[0];
-				y = event.values[1];
-				z = event.values[2];				
-			}
-			if(event.sensor.getType() == Sensor.TYPE_ORIENTATION){
-				orientation = -event.values[0];
-			}
-			String str = System.currentTimeMillis() + "," + x + "," + y + "," + z + "," + orientation + "\n";
-			try {
-				//fos = openFileOutput("sensorFile", Context.MODE_PRIVATE);
-				
-				Log.d("Test", fos.toString());
-				fos.write(str.getBytes());
-				//fos.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				xaxis.setText("didn't write");
-			}
-		}		
-	};
-    
 }
---
+++ src/edu/elon/cs/collection/GPSCollector.java
@@ -0,0 +1,98 @@
+package edu.elon.cs.collection;
+
+/**
+ *  Collects GPS data into a file.
+ * 
+ *  @author J. Hollingsworth
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+
+public class GPSCollector extends Thread {
+
+	private LocationManager locManager;
+	private boolean running;
+	private FileOutputStream outFile = null;
+	private Handler handler;
+
+	public GPSCollector(Context context, Handler handler) {
+		this.running = false;
+		this.handler = handler;
+
+		// GPS setup
+		locManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE); 
+		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10000.0f, locListener);
+
+		// File setup
+		File test = Environment.getExternalStorageDirectory();
+		try {
+			outFile = new FileOutputStream(new File(test, "gpsFile.csv"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
+	}
+
+	public void run() {
+
+		while (running) {
+			// do work here
+		}
+
+		locManager.removeUpdates(locListener);
+
+		try {
+			outFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private LocationListener locListener = new LocationListener() {
+
+		public void onLocationChanged(Location location) {
+
+			// only write the string if thread is running
+			if (running) {
+				String printOut = System.currentTimeMillis() + "," + location.getLatitude() + "," + location.getLongitude() + "," + location.getAccuracy();
+				try {
+					outFile.write(printOut.getBytes());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		//@Override
+		public void onProviderDisabled(String arg0) {}
+		//@Override
+		public void onProviderEnabled(String arg0) {}
+		//@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			if (status == LocationProvider.AVAILABLE) {
+				Message msg = handler.obtainMessage();
+				Bundle b = new Bundle();
+				b.putString("gps-message", "Available");
+				msg.setData(b);
+				handler.sendMessage(msg);
+			}
+		}
+	};
+}
