604ebd31fe65bcae84fd012b5f52a97fc4c4044f
Colleen <Colleen@10.112.73.43>
2012-07-24 12:35:03 -0400

    compassview class added

---
+++ src/edu/elon/cs/collection/CompassView.java
@@ -0,0 +1,34 @@
+package edu.elon.cs.collection;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import edu.elon.cs.collection.R;
+
+
+public class CompassView extends ImageView {
+
+	private float azimuth;
+	private float cx, cy;
+	
+	public CompassView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		azimuth = 0.0f;
+		Drawable image = getResources().getDrawable(R.drawable.compass);
+		cx = image.getIntrinsicWidth() / 2;
+		cy = image.getIntrinsicHeight() / 2;
+	}
+
+	public void setAzimuth(float azimuth){
+		this.azimuth = azimuth;
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas){
+		canvas.rotate(azimuth, cx, cy);
+		super.onDraw(canvas);
+	}
+}
